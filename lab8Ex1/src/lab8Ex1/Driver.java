5d0d412e956a444b4c5a06b620f31e1a049a808b
cmallen2106 <cmallen2106@gmail.com>
2021-08-23 14:15:57 +0100

    update to driver

--- lab8Ex1/src/lab8Ex1/Driver.java
+++ lab8Ex1/src/lab8Ex1/Driver.java
@@ -1,6 +1,6 @@
 //Christina Sosa
 //Programming Fundamentals - Summer 2021
-//Lab 8 - Exercise 1
+//Lab 8 - Exercise 1 - 3
 
 /*Exercise 1
 Modify the main method of the Driver class from Lab #7. In this method, do the following:
@@ -17,59 +17,75 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
-		// create the ArrayStack and LinkedStack
-		ArrayStack arrayStack = new ArrayStack(); 
-		LinkedStack linkedStack = new LinkedStack(); 
+		// create the ArrayQueue and LinkedQueue
+		ArrayQ arrayQ = new ArrayQ(); 
+		LinkedQueue linkedQ = new LinkedQueue(); 
 		
 		//pushing values into two stacks
 		
 		//first array stack
-		arrayStack.push(1); 
-		arrayStack.push(7); 
-		arrayStack.push(3); 
-		arrayStack.push(4); 
-		arrayStack.push(9); 
-		arrayStack.push(2); 
+		arrayQ.enqueue(1); 
+		arrayQ.enqueue(7); 
+		arrayQ.enqueue(3); 
+		arrayQ.enqueue(4); 
+		arrayQ.enqueue(9); 
+		arrayQ.enqueue(2); 
 		
 		//second linked stack
-		linkedStack.push(1); 
-		linkedStack.push(7); 
-		linkedStack.push(3); 
-		linkedStack.push(4);
-		linkedStack.push(9);
-		linkedStack.push(2); 
+		linkedQ.enqueue(1); 
+		linkedQ.enqueue(7); 
+		linkedQ.enqueue(3); 
+		linkedQ.enqueue(4);
+		linkedQ.enqueue(9);
+		linkedQ.enqueue(2); 
 		
-		//pop off elements from ArrayStack and display each int as it's removed
-		while (!arrayStack.isEmpty()) {
-			System.out.println("ArrayStack: " + arrayStack.pop());
+		//pop off and print the values until array is empty
+		while (!arrayQ.isEmpty()) {
+			System.out.println("ArrayQueue: " + arrayQ.dequeue());
 		}
 
 		System.out.println("\n");
 		
-		//pop off elements from LinkedStack and display each int as it's removed
-		while (!linkedStack.isEmpty()) {
-			System.out.println("LinkedStack: " + linkedStack.pop());
+		while (!linkedQ.isEmpty()) {
+			System.out.println("LinkedQueue: " + linkedQ.dequeue());
 		}
 		
 		System.out.println("\n");
 		
-		//create new LinkedStack
-		LinkedStack linkedStack2 = new LinkedStack(); 
+		/*
+		 * Modify the LinkedQueue class to include a new method called removeMiddle, which
+		 * removes from the queue the value that is in the middle.
+		 * For example, if the queue contains 3, 7, 2, 4, 5, then 2 will be removed.
+		 * If the queue contains 2, 9, 1, 4 (even number of values), then remove 9 (round down).
+		 * Test the method using the Driver program.
+		 */
 		
-		linkedStack2.push(1);
-		linkedStack2.push(7);
-		linkedStack2.push(3);
-		linkedStack2.push(4);
-		linkedStack2.push(9);
-		linkedStack2.push(2);		
+		//create new LinkedQueue for Exercise 2
+		LinkedQueue linkedQ2 = new LinkedQueue(); 
 		
-		//connect removeBottomHalf method from the LinkedStack class
-		linkedStack2.removeBottomHalf(); 
+		linkedQ2.enqueue(1);
+		linkedQ2.enqueue(7);
+		linkedQ2.enqueue(3);
+		linkedQ2.enqueue(4);
+		linkedQ2.enqueue(9);
+		linkedQ2.enqueue(2);		
 		
-		//print remaining elements
-		System.out.println("After removing bottom half: " + linkedStack2);
+		//printing out without the middle element
+		System.out.println("Removed Middle Element: "); 
+		linkedQ2.removeMiddle(); 
 		
-		//Time complexity is O(N)
+		/*
+		 * Exercise 3
+		 * Use the tree diagram below to answer the questions below.
+		 * a) What is the root node of the tree?
+		 * 		50
+		 * b) What are the leaf nodes of the tree?
+		 * 		9, 14, 19, 67, 76
+		 * c) What are the ancestor nodes of the node containing 19?
+		 * 		23, 17, 50
+		 * d) Write the sequence of node values that you would get from a post-order traversal.
+		 * 		9, 14, 12, 19, 23, 17, 67, 54, 76, 72, 50
+		 */
 		
 
 	}
